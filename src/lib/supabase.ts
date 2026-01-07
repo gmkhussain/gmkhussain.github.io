443fc2b036608c9822b9197a2c7ceaabbd2b8849
hussain <gmk.hussain@gmail.com>
2026-01-08 01:01:37 +0500

    fix url

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -4,4 +4,4 @@ import { createClient } from "@supabase/supabase-js";
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const supabaseKey = import.meta.env.VITE_SUPABASE_PUBLISHABLE_DEFAULT_KEY;
 
-export const supabase = createClient(btoa(supabaseUrl), btoa(supabaseKey));
+export const supabase = createClient(atob(supabaseUrl), atob(supabaseKey));
