5e11d0107d792af541e080e1f66f1f2744cf877d
hussain <gmk.hussain@gmail.com>
2026-01-08 00:57:59 +0500

    add layer on key

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -4,4 +4,4 @@ import { createClient } from "@supabase/supabase-js";
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const supabaseKey = import.meta.env.VITE_SUPABASE_PUBLISHABLE_DEFAULT_KEY;
 
-export const supabase = createClient(supabaseUrl, supabaseKey);
+export const supabase = createClient(btoa(supabaseUrl), btoa(supabaseKey));
