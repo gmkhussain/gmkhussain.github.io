3d840b4e4687f62a1384c74b90a25d77a47b9c6f
hussain <gmk.hussain@gmail.com>
2026-01-08 10:56:29 +0500

    url

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,7 +1,9 @@
 
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
-const supabaseKey = import.meta.env.VITE_SUPABASE_PUBLISHABLE_DEFAULT_KEY;
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL || `Imh0dHBzOi8vb2ZuZXZzd3d0aWV2cWhsbmpyZXouc3VwYWJhc2UuY28i`;
+const supabaseKey = import.meta.env.VITE_SUPABASE_PUBLISHABLE_DEFAULT_KEY || `InNiX3B1Ymxpc2hhYmxlX1Y3M0VVOXRvUXBla0hHSTVtTTJKX1FfZTJCX1d1THUi`;
 
-export const supabase = createClient(atob(supabaseUrl), atob(supabaseKey));
+const clr =( str => JSON.parse(atob(str)))
+
+export const supabase = createClient(clr(supabaseUrl), clr(supabaseKey));
